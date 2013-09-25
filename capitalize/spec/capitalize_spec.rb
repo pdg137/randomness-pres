4ee59f1efdcbf1dfb973e0de07fb9d3eb4045a47
Paul Grayson <paul@pololu.com>
2013-09-24 20:08:53 -0700

    updated presentation and code

--- capitalize/spec/capitalize_spec.rb
+++ capitalize/spec/capitalize_spec.rb
@@ -9,17 +9,26 @@ describe Capitalize do
       end
     end
 
-    context "given 'blah\n\blah\nBLAH'" do
+    context "given 'blah\nblah\nBLAH'" do
       let(:s) { "blah\nblah\nBLAH" }
       it "should return 'Blah\nBlah\nBLAH'" do
         Capitalize.capitalize_each_line(s).should == "Blah\nBlah\nBLAH"
+      end
+    end
+
+    100.times do
+      s = 10.times.map { rand(128).chr }.join # random 10-character ASCII string
+      context "given '#{s.inspect}" do
+        it "should give a result that downcases to the same value" do
+          Capitalize.capitalize_each_line(s).downcase.should == s.downcase
+        end
       end
     end
   end
 
   describe "capitalize_file" do
     context "given a file containing 'abcd'" do
-      before(:each) do
+      before do
         File.write("file.txt", "abcd")
       end
 
@@ -29,11 +38,11 @@ describe Capitalize do
     end
 
     context "given a missing file" do
-      before(:each) do
+      before do
         File.unlink("file.txt")
       end
 
-      it "should raise an excpetion" do
+      it "should raise an exception" do
         expect { Capitalize.capitalize_file("file.txt") }.to raise_exception
       end
     end
--- capitalize/spec/fuzz.rb
+++ capitalize/spec/fuzz.rb
@@ -1,5 +1,5 @@
-    10.times do
-      s = (0...10).map { rand(128).chr }.join
+    100.times do
+      s = 10.times.map { rand(128).chr }.join # random 10-character ASCII string
       context "given '#{s.inspect}" do
         it "should give a result that downcases to the same value" do
           Capitalize.capitalize_each_line(s).downcase.should == s.downcase
