341515ec25c1200521db54b8543316923f655eb0
Paul Grayson <paul@pololu.com>
2013-09-24 20:10:30 -0700

    removed fuzz from actual spec (for presentation)

--- capitalize/spec/capitalize_spec.rb
+++ capitalize/spec/capitalize_spec.rb
@@ -15,15 +15,6 @@ describe Capitalize do
         Capitalize.capitalize_each_line(s).should == "Blah\nBlah\nBLAH"
       end
     end
-
-    100.times do
-      s = 10.times.map { rand(128).chr }.join # random 10-character ASCII string
-      context "given '#{s.inspect}" do
-        it "should give a result that downcases to the same value" do
-          Capitalize.capitalize_each_line(s).downcase.should == s.downcase
-        end
-      end
-    end
   end
 
   describe "capitalize_file" do
