a1559d0babdc33d034f8ada2a096e83e2c84f545
Paul Grayson <paul@pololu.com>
2013-09-25 06:58:39 -0700

    reduce fuzz to 20

--- capitalize/spec/fuzz.rb
+++ capitalize/spec/fuzz.rb
@@ -1,4 +1,4 @@
-    100.times do
+    20.times do
       s = 10.times.map { rand(128).chr }.join # random 10-character ASCII string
       specify "given #{s.inspect}, returns a result that downcases to the same value" do
         Capitalize.capitalize_each_line(s).downcase.should == s.downcase
