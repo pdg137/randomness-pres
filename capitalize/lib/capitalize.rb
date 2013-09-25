38f9378ee9374b24a1c8a747d23b9af3f84f0669
Paul Grayson <paul@pololu.com>
2013-09-24 20:12:48 -0700

    simplified code

--- capitalize/lib/capitalize.rb
+++ capitalize/lib/capitalize.rb
@@ -1,8 +1,7 @@
 class Capitalize
   # Part one (fuzz testing)
   # Given a string, capitalize the first letter of each line.
-  def self.capitalize_each_line(line)
-    s = line.dup
+  def self.capitalize_each_line(s)
     s.split("\n").map { |line|
       line[0] = line[0].upcase
       line
