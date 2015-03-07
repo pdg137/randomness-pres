aab8129ef966b8f3e91ed5eb4492782182e60b5c
Paul Grayson <pdg@alum.mit.edu>
2015-03-06 23:50:22 -0800

    empty code for kata

--- capitalize/spec/capitalize_spec.rb
+++ capitalize/spec/capitalize_spec.rb
@@ -2,16 +2,5 @@ require 'spec_helper'
 
 describe Capitalize do
   describe "capitalize_each_line" do
-    specify "given 'abcd' returns 'Abcd'" do
-      expect(Capitalize.capitalize_each_line('abcd')).to eq 'Abcd'
-    end
-
-    specify "given '\nabcd' returns '\nAbcd'" do
-      expect(Capitalize.capitalize_each_line("\nabcd")).to eq "\nAbcd"
-    end
-
-    specify "given 'blah\nblah\nBLAH' should return 'Blah\nBlah\nBLAH'" do
-      expect(Capitalize.capitalize_each_line("blah\nblah\nBLAH")).to eq "Blah\nBlah\nBLAH"
-    end
   end
 end
